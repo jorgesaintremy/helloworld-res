17c4d627267d505e675e57b1b8dd2c9f444cab6b
jorgesaintremy <jorgesaintremy@gmail.com>
2026-01-18 18:53:13 +0100

    Update calc_test.py

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -21,20 +21,27 @@ class TestCalculate(unittest.TestCase):
         self.assertEqual(0, self.calc.add(-2, 2))
         self.assertEqual(1, self.calc.add(1, 0))      
         
-    def test_divide_method_returns_correct_result(self):
-        self.assertEqual(1, self.calc.divide(2, 2))
-        self.assertEqual(1.5, self.calc.divide(3, 2))
-        self.assertEqual(-1.5, self.calc.divide(3, -2))
-        self.assertEqual(0, self.calc.divide(0, 2))
+    def test_divide_by_zero_method(self):
         self.assertRaises(TypeError, self.calc.divide, 2, 0)
         self.assertRaises(TypeError, self.calc.divide, 2, -0)
-        self.assertRaises(TypeError, self.calc.divide, 2, 0.0)
+        self.assertRaises(TypeError, self.calc.divide, 10, 0.0)
         self.assertRaises(TypeError, self.calc.divide, 2, -0.0)
         self.assertRaises(TypeError, self.calc.divide, 2, "0")
         self.assertRaises(TypeError, self.calc.divide, 2, "-0")
         self.assertRaises(TypeError, self.calc.divide, 2, "0.0")
         self.assertRaises(TypeError, self.calc.divide, 2, "-0.0")
-        self.assertRaises(TypeError, self.calc.divide, "2", 2)
+    
+    def test_divide_method_returns_correct_result(self):
+        self.assertEqual(1, self.calc.divide(2, 2))
+        self.assertEqual(1.5, self.calc.divide(3, 2))
+        self.assertEqual(-1.5, self.calc.divide(3, -2))
+        self.assertEqual(0, self.calc.divide(0, 2))
+
+    def test_divide_negative_numbers_method_returns_correct_result(self):
+        self.assertEqual(1, self.calc.divide(-2, -2))
+        self.assertEqual(-1.5, self.calc.divide(-3, 2))
+        self.assertEqual(-1.5, self.calc.divide(3, -2))
+        self.assertEqual(0, self.calc.divide(0, -2))
   
     def test_add_method_fails_with_nan_parameter(self):
         self.assertRaises(TypeError, self.calc.add, "2", 2)
